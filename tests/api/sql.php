cfb33c12a90bdac6cd226d879f73246bf56dcd48
wuqingbao <wuxiumu@163.com>
2022-01-10 01:39:19 +0800

    api sql config

--- tests/api/sql.php
+++ tests/api/sql.php
@@ -2,22 +2,25 @@
 
 // 如果你使用php的依赖安装。可以使用以下方法自动载入
 require '../../vendor/autoload.php';
- 
+
 use Medoo\Medoo;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../../');
+$dotenv->load();
 
 // 初始化配置
 $database = new Medoo([
-    'database_type' => 'mysql',
-    'database_name' => 'sql50',
-    'server' => '127.0.0.1',
-    'username' => 'root',
-    'password' => '123456',
+    'database_type' => $_ENV['DB_CONNECTION'],
+    'server' => $_ENV['DB_HOST'],
+    'database_name' => $_ENV['DB_DATABASE'],
+    'username' => $_ENV['DB_USERNAME'],
+    'password' => $_ENV['DB_PASSWORD'],
     'charset' => 'utf8'
 ]);
 $data['code']=200;
-$data['msg']='ok'; 
+$data['msg']='ok';
 $data['data'] = $database->query($_POST['content'])->fetchAll();
- 
+
 header('Content-Type:application/json; charset=utf-8');
 
 echo json_encode($data);
